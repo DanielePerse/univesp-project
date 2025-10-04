d7fa96f866d86954712460deba4957cb52987383
DanielePerse <danieleperse3@gmail.com>
2025-10-04 15:52:01 -0300

    feat: mvc architecture

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
-    app = Flask(__name__, template_folder='frontend', static_folder='frontend')
+    app = Flask(__name__, template_folder='templates', static_folder='static')
     app.config.from_object(Config)
     
     db.init_app(app)
